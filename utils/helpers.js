f9b3c29197c1bddfce28479dcd2aea42ae1c85ed
TheWeeezel <d.richner@vedea.ch>
2019-04-26 15:35:19 +0200

    feat: changed helpers file

--- utils/helpers.js
+++ utils/helpers.js
@@ -47,7 +47,7 @@ export function setLocalNotification() {
       if (data === null) {
         Permissions.askAsync(Permissions.NOTIFICATIONS).then(({ status }) => {
           if (status === "granted") {
-            Notifications.cancelAllScheduledNotificationsAsync();
+            Notifications.cancelAllScheduledNotificationAsync();
 
             let tomorrow = new Date();
             tomorrow.setDate(tomorrow.getDate() + 1);
